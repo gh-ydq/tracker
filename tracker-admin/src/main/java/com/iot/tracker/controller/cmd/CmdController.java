b9feb7287401a42983861179b1c77e2d63343c12
76542048 <765420482@qq.com>
2017-09-20 18:23:24 +0800

    命令配置

--- tracker-admin/src/main/java/com/iot/tracker/controller/cmd/CmdController.java
+++ tracker-admin/src/main/java/com/iot/tracker/controller/cmd/CmdController.java
@@ -1,5 +1,21 @@
 package com.iot.tracker.controller.cmd;
 
-public class CmdController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.qdigo.iotsdk.DeviceCtl;
+
+@RequestMapping(value="/cmd")
+@RestController
+public class CmdController {
+	private Logger logger = LoggerFactory.getLogger(CmdController.class);
+	@RequestMapping(value="/deviceSwitch")
+	public void deviceSwitch(@RequestParam("powerSwitch") String powerSwitch,@RequestParam("imei") long imei){
+		logger.info("发送下行命令powerSwitch={},imei={}",powerSwitch,imei);
+		DeviceCtl deviceCtl = new DeviceCtl();
+		deviceCtl.deviceStart(imei);
+	}
 }
